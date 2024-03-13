d56ca6124478f28d1a7d56b33b6d8588af1e0e51
Alex McGuinness <github@alexmcguinness.me.uk>
2024-03-13 21:33:43 +0000

    add DB integration

---
+++ film_api.py
@@ -0,0 +1,93 @@
+
+import logging
+import requests
+import json
+
+
+class FilmAPI:
+   """The Film API"""
+
+   #DATABASE_NAME = "FilmDatabase"
+#   url = "http://192.168.0.160"
+#   port = "80"
+#   url = "http://192.168.0.121"
+#   port = "8080"
+   protocol = "http://"
+
+   film_lookup = 0
+   film_found = 0
+   film_added = 0
+
+
+   def __init__(self, server, port):
+      """Init"""
+      self.server = server
+      self.port = port
+#      logging.debug("Connecting to "+ self.server+ ":" + self.port + " with user:" + self.user + " and " + self.password)
+
+
+#   def add_film(self, title, year, imdbid=None, runtime=None, classification=None, rating=None, media_type=None):
+   def add_film(self, title, year, media_type, details):
+      """Add film to database"""
+ 
+      endpoint = "/api/film"
+      jobj = {}
+      jobj["title"] = title
+      jobj["year"] = year
+ 
+      if "imdbid" in details:
+         jobj["imdbid"] = details["imdbid"]
+      #if runtime: 
+      if "runtime" in details:
+         jobj["runtime"] = details["runtime"]
+      #if classification: 
+      if "classification:" in details:
+         jobj["classification"] = details["classification"]
+      #if rating: 
+      if "rating:" in details:
+         jobj["rating"] = details["rating"]
+      if media_type: 
+         jobj["media_type"] = media_type
+
+      logging.debug("Adding Film:" + str(jobj))
+      headers = {"Content-Type": "application/json"}
+      logging.debug("obj:" + str(jobj))
+
+      #output = requests.post(self.protocol + self.server + ":" +self.port + endpoint, json=jobj, headers=headers)
+      self.film_added += 1
+      #logging.debug("Added Film:" + str(output))
+
+
+   def get_film(self, film_name, film_year):
+      """Looking for film with name / year """
+  
+      self.film_lookup += 1
+ 
+      endpoint = "/api/film"
+
+      jobj = {}
+      jobj["title"] = film_name
+      jobj["year"] = film_year
+
+      logging.debug("Looking for Film:" + str(film_name) + " Year:" + str(film_year))
+      headers = {"Content-Type": "application/json"}
+      logging.debug("obj:" + str(jobj))
+      output = requests.get(self.protocol + self.server + ":" +self.port + endpoint, params=jobj, headers=headers)
+      logging.debug("Returned:" + str(output))
+      logging.debug("  Body:" + str(output.content))
+
+      rc = {}
+
+      if output.status_code == 200:
+         logging.debug("Status:" + str(output.status_code))
+         self.film_found += 1
+ 
+         rc = json.loads(output.content.decode("utf-8"))
+
+      return rc
+
+   def print_stats(self):
+      print("DB Stats")
+      print("Title Lookups:" + str(self.film_lookup))
+      print("Films Found  :" + str(self.film_found))
+      print("Films Added  :" + str(self.film_added))
--- get_titles_from_disk.py
+++ get_titles_from_disk.py
@@ -8,7 +8,14 @@ import re
 #from film_manager.film_uploader import FilmUploader
 #from network.script_record import ScriptRecordTable
 
-from film_database import FilmDatabase
+from lookup_imdb import get_titles
+from lookup_imdb import get_details_from_title_year
+from lookup_imdb import print_stats as print_imdb_stats
+
+from film_api import FilmAPI
+
+#from film_database import FilmDatabase
+#from film_database import FilmDatabase
 
 from file_system import FileSystem
 
@@ -32,7 +39,6 @@ def main(argv):
    except getopt.GetoptError:
       usage()
       sys.exit(2)
-
 
    loglevel = "DEBUG"
    num_lookups = -1
@@ -61,24 +67,27 @@ def main(argv):
    console.setFormatter(formatter)
 #   logging.getLogger('').addHandler(console)
 
-
-
-
    logging.info("Storing titles from File System")
 
    smb_user = os.environ["SMB_USER"]
    smb_server = os.environ["SMB_HOST"]
    smb_password = os.environ["SMB_PASS"]
 
-   mongo_user = os.environ["MONGODB_USER"]
-   mongo_pass = os.environ["MONGODB_PASS"]
-   mongo_server = os.environ["MONGODB_HOST"]
-   mongo_port = os.environ["MONGODB_PORT"]
+#   mongo_user = os.environ["MONGODB_USER"]
+#   mongo_pass = os.environ["MONGODB_PASS"]
+#   mongo_server = os.environ["MONGODB_HOST"]
+#   mongo_port = os.environ["MONGODB_PORT"]
 
-   db = FilmDatabase(mongo_user, mongo_pass, mongo_server, mongo_port)
+#   db = FilmDatabase(mongo_user, mongo_pass, mongo_server, mongo_port)
+ 
+   server = "192.168.0.160"
+   port = "80"
+   api = FilmAPI(server, port)
    fs = FileSystem(smb_server, smb_user, smb_password)
    br_dir = "\Films\BluRay"
    dvds_dir = "\Films\DVDs"
+   files_dir = "\Films\Files"
+   offline_dir = "\Films\Offline"
 
 #   import time
 #   time.sleep(600)
@@ -87,10 +96,14 @@ def main(argv):
    dvd_titles = fs.listDir(dvds_dir)
 
    #logging.debug("Titles:" + str(titles))
+   #logging.debug("Titles:" + str(titles[0]))
 
    film_pattern = "(^[^\(]*) \(([^\)]*)"
 
+   num = 0
    for btitle_year in br_titles:
+      if num >= 2:
+         break
       logging.debug("CHECK:" + btitle_year)
       res = re.match(film_pattern, btitle_year)
       if res:
@@ -98,46 +111,27 @@ def main(argv):
          year = res.group(2)
          logging.debug("MATCH TITLE:" + title) 
          logging.debug("       YEAR:" + year)
-         db.add_bluray(title, year)
 
-   for dtitle_year in dvd_titles:
-      logging.debug("CHECK:" + dtitle_year)
-      res = re.match(film_pattern, dtitle_year)
-      if res:
-         title = res.group(1)
-         year = res.group(2)
-         logging.debug("MATCH TITLE:" + title) 
-         logging.debug("       YEAR:" + year)
-         db.add_dvd(title, year)
+         res = api.get_film(title, year)
+         logging.debug("Found in DB:" + str(res))
+         logging.debug("len Res:" + str(len(res)))
 
+         if len(res) > 0 and res[0]["imdbid"] != None:
+            logging.debug("First Film in DB [0]:" + str(res[0]))
+            logging.debug("Found film in DB")
+         else:
+            logging.debug("Not Found in DB")
+            details = get_details_from_title_year(title, year)
+            if len(details):
+               logging.debug("Found in IMDB:" + str(details))
+               api.add_film(title, year, "BluRay", details)
+       
+ #        db.add_bluray(title, year)
+         num += 1
 
+   print_imdb_stats()
+   api.print_stats()
 
-   
-
-
-
-
-#   if max_records:
-#      film_uploader = FilmUploader(num_lookups, max_records)
-#   else:
-#      film_uploader = FilmUploader(num_lookups)
-#
-#   film_uploader.upload_films_from_file_system()
-#   film_uploader.print_file_update_result()
-#
-#   status = film_uploader.get_exit_status()
-#   summary = film_uploader.get_summary()
-#   #logging.debug("summary:" + str(summary))
-#   #logging.debug("summary len:" + str(len(str(summary))))
-#   #logging.debug(__file__)
-
-   #store in database (__file__, status, summary)
-#   script_records = ScriptRecordTable()
-
-#   summary="this is a summary"
-
-#   scriptname = os.path.basename(__file__)
-#   script_records.add(scriptname, status, summary)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
